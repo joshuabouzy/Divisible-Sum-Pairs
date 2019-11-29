84afd5744c71591efef0eb63f87489bdfd94f857
jbouzy-laptop <joshbouzy@gmail.com>
2019-11-29 15:54:53 -0500

    Initial commit
    Added image and timers

--- Divisible Sum Pairs/Divisible_Sum_Pairs.py
+++ Divisible Sum Pairs/Divisible_Sum_Pairs.py
@@ -1,4 +1,14 @@
+import os
+import sys
+import time
 import itertools
+from os import system, name    
+
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls')   
+    else: 
+        _ = system('clear')
 
 def divisibleSumPairs(divisor, numbers):
     countOfPairs = 0
@@ -10,10 +20,38 @@ def divisibleSumPairs(divisor, numbers):
         iterator+=1
     return(countOfPairs) 
 
+print(r"""
+
+      _,,ddF```Ybb,,_
+    ,d@#@#@#@g,   ``Yb,
+  ,d#@#V``V@#@#b      `b,
+ d@#@#I    I@#@8        `b
+d@#@#@#A..A@#@#P         `b
+8#@#@#@#@#@#@8`           8
+8@#@#@#@#@#@J             8
+8#@#@#@#@#P               8
+Y@#@#@#@#P    ,db,       ,P
+ Y@#@#@#@)    @DWB      aP
+  `Y#@#@#b    `69`    aP`
+    `Y@#@#g,,     _,dP`
+      ```YBBgggddP```
+
+Challenge: Divisible Sum Pairs
+
+ASCII Artist: Donovan Bake @ https://www.asciiart.eu/
+      
+  """)
+
+time.sleep(5)
+clear() 
+time.sleep(2)
+
 divisor = int(input("Input the divisor: "))
 
-numbers = list(map(int, input("Input the list of numbers with a space in between each: ").rstrip().split()))
+numbers = list(map(int, input("\nInput the list of numbers with a space in between each: ").rstrip().split()))
 
 result = divisibleSumPairs(divisor, numbers)
 
-print("There are " + str(result) + " pairs.\n")
+print("\nThere are " + str(result) + " divisible pairs.\n")
+
+time.sleep(10)
